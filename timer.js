dc4f6df6e4bbc63b987956e2292f1939884418ff
Ram Lakhan Singh <62960598+lakhan5555@users.noreply.github.com>
2020-10-19 18:36:55 +0530

    Add files via upload

---
+++ timer.js
@@ -0,0 +1,45 @@
+function showTimer(){
+	var time = "00: 01: 00";
+	timer_div = document.getElementById('timer_div');
+	timer_div.innerHTML = time;
+	my_timer = setInterval(function(){
+		var hr = 0;
+		var min = 0;
+		var sec = 0;
+		var time_up = false;
+		t = time.split(":");
+		hr = parseInt(t[0]);
+		min = parseInt(t[1]);
+		sec = parseInt(t[2]);
+		if(sec == 0) {
+			if(min > 0){
+				sec = 59;
+				min--;
+			}
+			else if(hr > 0){
+				min = 59;
+				sec = 59;
+				hr--;
+			}
+			else{
+				time_up = true;
+			}
+		}	
+		else{
+			sec--;
+		}	
+		if(hr < 10)
+			hr = "0" + hr;
+		if(min < 10)
+			min = "0" + min;
+		if(sec < 10)
+			sec = "0" + sec;
+		time = hr + " :" + min + " :" + sec;
+		if(time_up)
+			time = "TIME UP";
+		timer_div = document.getElementById("timer_div");
+		timer_div.innerHTML = time;
+		if(time_up)
+			clearInterval(my_timer);
+	}, 1000);
+}
